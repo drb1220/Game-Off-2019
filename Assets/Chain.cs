c9aa8f001df9eca7a9cbc721e537319b95759ef1
drb1220 <david.1220.bunger@gmail.com>
2019-11-03 14:28:23 -0500

    Semi Working Ball

---
+++ Assets/Chain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    public Rigidbody2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            player.velocity = new Vector2(0, 0);
+        }
+    }
+}
