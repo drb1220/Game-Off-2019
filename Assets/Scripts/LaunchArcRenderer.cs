0434110f61ffac3b43ae9c68486c6208b2ad501f
drb1220 <david.1220.bunger@gmail.com>
2019-11-07 16:07:17 -0500

    Starting work on arc

---
+++ Assets/Scripts/LaunchArcRenderer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class LaunchArcRenderer : MonoBehaviour
+{
+    LineRenderer lr;
+
+    public float velocity;
+    public float angle;
+    public int resolution = 10;
+
+    float g;
+    float radianAngle;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.useWorldSpace = false;
+        g = Mathf.Abs(Physics2D.gravity.y);
+    }
+    void FixedUpdate()
+    {
+        RenderArc();
+    }
+
+    void RenderArc()
+    {
+        lr.SetVertexCount(resolution + 1);
+        lr.SetPositions(CalculateArcArray());
+
+    }
+
+    Vector3[] CalculateArcArray()
+    {
+        Vector3[] arcArray = new Vector3[resolution + 1];
+        radianAngle = Mathf.Deg2Rad * angle;
+        float maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / g;
+
+        for (int i = 0; i <= resolution; i++)
+        {
+            float t = (float)i / (float)resolution;
+            arcArray[i] = CalculateArcPoint(t, maxDistance);
+        }
+        return arcArray;
+    }
+
+
+    Vector3 CalculateArcPoint(float t, float maxDistance)
+    {
+        float x = t * maxDistance;
+        float y = x * Mathf.Tan(radianAngle) - ((g * x * x) / (2 * velocity * velocity * Mathf.Cos(radianAngle) * Mathf.Cos(radianAngle)));
+        return new Vector3(x, y);
+    }
+
+    
+}
